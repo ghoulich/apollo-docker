76ae35ebcf53ab75098d413b26f4dc6d0c35090a
ghoulich <ghoulich@hotmail.com>
2018-04-26 20:44:50 +0800

    Update start-apollo.sh

--- apollo-compose/start-apollo.sh
+++ apollo-compose/start-apollo.sh
@@ -7,9 +7,11 @@ cp my.cnf /usr/local/mysql/config/
 chown -R 27:27 /usr/local/mysql
 
 # 获取本机IP地址
-host_ip=$(ifconfig ens33 | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:")
+host_ip=$(ifconfig ens3 | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:")
 
 # 向docker-compose.yml添加主机IP地址
+rm -rf docker-compose.yml
+cp docker-compose.yml.bak docker-compose.yml
 sed -i "12a\      HOST_IP: ${host_ip}" docker-compose.yml
 
 # 启动Apollo容器集群
